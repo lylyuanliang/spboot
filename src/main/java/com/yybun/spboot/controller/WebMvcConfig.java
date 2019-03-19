61ad2143547dd36c885dc606c9eb611c8a505f99
liurl <lylyuanliang@.com>
2019-03-20 00:34:41 +0800

    登陆

---
+++ src/main/java/com/yybun/spboot/controller/WebMvcConfig.java
@@ -0,0 +1,15 @@
+package com.yybun.spboot.controller;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+}
